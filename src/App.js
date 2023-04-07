a915db987922481d68ec759a5b44f125cca3e31d
dongwook98 <ehddnr8563@gmail.com>
2023-04-07 16:09:44 +0900

    fix : 쓸데없는 css, div태그 삭제

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ const filters = ['all', 'active', 'completed'];
 function App() {
   const [filter, setFilter] = useState(filters[0]);
   return (
-    <div>
+    <>
       <Header
         filter={filter}
         filters={filters}
         onFilterChange={setFilter}
       ></Header>
       <TodoList filter={filter}></TodoList>
-    </div>
+    </>
   );
 }
 
